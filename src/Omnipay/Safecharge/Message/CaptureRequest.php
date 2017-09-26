e4e9b78b151b8188e0036c0f68e33b6d87abd76c
Lubo Anev <lubo.anev@siteground.com>
2017-09-26 11:24:02 +0300

    Adding missing parameters to capture functionality.

--- src/Omnipay/Safecharge/Message/CaptureRequest.php
+++ src/Omnipay/Safecharge/Message/CaptureRequest.php
@@ -9,21 +9,17 @@ class CaptureRequest extends AbstractRequest
 {
     public function getData()
     {
-        $data = parent::getData();
-
         $this->validate('amount', 'currency');
 
+        $data = parent::getData();
         $data['sg_TransType'] = 'Settle';
-
-        $this->validate('authCode', 'transactionReference');
-
+        $data['sg_CCToken'] = $this->getToken();
+        $data['sg_ExpMonth'] = $this->getExpMonth();
+        $data['sg_ExpYear'] = $this->getExpYear();
         $data['sg_AuthCode'] = $this->getAuthCode();
-        $data['sg_TransactionID'] = (int) $this->getTransactionReference();
+        $data['sg_TransactionID'] = (int) $this->getTransactionId();
         $data['sg_Amount'] = $this->getAmount();
         $data['sg_Currency'] = $this->getCurrency();
-
-//        var_dump($data);
-//        exit;
 
         return $data;
     }
